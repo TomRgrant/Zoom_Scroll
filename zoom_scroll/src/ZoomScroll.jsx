e7225e9209e86f2f3a8ad7eaf451eeb19ca48216
Tom Grant <grntthomasronnie@gmail.com>
2023-07-17 13:39:18 +0100

    Added scalling to divs based on scroll

--- zoom_scroll/src/ZoomScroll.jsx
+++ zoom_scroll/src/ZoomScroll.jsx
@@ -3,15 +3,22 @@ import './ZoomScroll.css'
 
 const ZoomScroll = () => {
 
+    let num = 1
 
-
-
-
+const scroll = (e) => {
+    console.log(num);
+    document.documentElement.style.setProperty("--div-scale", num);
+    if (e.deltaY > 0){
+        num = num + 0.1;
+    } else {
+        num = num - 0.1;
+    }
+}
 
 
     return(
         <>
-        <div className="float-area">
+        <div onWheel={scroll} className="float-area">
             <div className="flo-div">
                 <h2>This is the floating text</h2>
             </div>
