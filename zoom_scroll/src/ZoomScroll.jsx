97076f2707d09a8f5132366f380e1f3a1603fddc
Tom Grant <grntthomasronnie@gmail.com>
2023-07-17 12:07:22 +0100

    Added basic divs and div postion

---
+++ zoom_scroll/src/ZoomScroll.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './ZoomScroll.css'
+
+const ZoomScroll = () => {
+
+
+
+
+
+
+
+    return(
+        <>
+        <div className="float-area">
+            <div className="flo-div">
+                <h2>This is the floating text</h2>
+            </div>
+
+            <div className="flo-div">
+                <h2>This is the floating text</h2>
+            </div>
+
+            <div className="flo-div">
+                <h2>This is the floating text</h2>
+            </div>
+
+            <div className="flo-div">
+                <h2>This is the floating text</h2>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default ZoomScroll;
