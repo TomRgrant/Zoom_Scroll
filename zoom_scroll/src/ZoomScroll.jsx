0e992f8589956bb6be140ecbfc89c82c5d7cd5ca
Tom Grant <grntthomasronnie@gmail.com>
2023-07-18 09:38:07 +0100

    Added x and y direction to divs and refactored code

--- zoom_scroll/src/ZoomScroll.jsx
+++ zoom_scroll/src/ZoomScroll.jsx
@@ -1,94 +1,76 @@
 import React from "react";
-import './ZoomScroll.css'
+import './ZoomScroll.css';
 
 const ZoomScroll = () => {
+    let num = 1;
+    const moveAmount1 = 5;
+    const moveAmount2 = 3;
+    const moveAmount3 = 7;
+    const moveAmount4 = 8;
 
-    let num = 1
+    console.log(parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-1-x")));
 
-    console.log(parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-1")));
+    const scroll = (e) => {
+        if (e.deltaY > 0) {
+            const currentScale = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--div-scale"));
+            if (currentScale + 0.1 < 2) {
+                num = currentScale + 0.1;
+                document.documentElement.style.setProperty("--div-scale", num.toString());
 
-const scroll = (e) => {
-
-    if (e.deltaY > 0) {
-        const currentScale = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--div-scale"));
-        if (currentScale + 0.1 < 2) {
-          num = currentScale + 0.1;
-          document.documentElement.style.setProperty("--div-scale", num.toString());
-
-          let currentPosition = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-1"))
-    console.log(currentPosition);
-    currentPosition = currentPosition + 5;
-    document.documentElement.style.setProperty("--move-div-1", `${currentPosition.toString()}%`)
-
-    let currentPosition2 = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-2"))
-    console.log(currentPosition2);
-    currentPosition2 = currentPosition2 - 2;
-    document.documentElement.style.setProperty("--move-div-2", `${currentPosition2.toString()}%`)
-
-    let currentPosition3 = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-3"))
-    console.log(currentPosition3);
-    currentPosition3 = currentPosition3 - 6;
-    document.documentElement.style.setProperty("--move-div-3", `${currentPosition3.toString()}%`)
-
-    let currentPosition4 = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-4"))
-    console.log(currentPosition4);
-    currentPosition4 = currentPosition4 + 8;
-    document.documentElement.style.setProperty("--move-div-4", `${currentPosition4.toString()}%`)
-    }
+                updatePosition(1, moveAmount1, true);
+                updatePosition(2, moveAmount2, false);
+                updatePosition(3, moveAmount3, true);
+                updatePosition(4, moveAmount4, false);
+            }
         }
 
-if (e.deltaY < 0) {
-    const currentScale = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--div-scale"));
-    if (currentScale - 0.1 > 0.1) {
-      num = currentScale - 0.1;
-      document.documentElement.style.setProperty("--div-scale", num.toString());
+        if (e.deltaY < 0) {
+            const currentScale = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--div-scale"));
+            if (currentScale - 0.1 > 0.1) {
+                num = currentScale - 0.1;
+                document.documentElement.style.setProperty("--div-scale", num.toString());
 
-      let currentPosition = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-1"))
-    console.log(currentPosition);
-    currentPosition = currentPosition - 5;
-    document.documentElement.style.setProperty("--move-div-1", `${currentPosition.toString()}%`)
-
-    let currentPosition2 = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-2"))
-    console.log(currentPosition2);
-    currentPosition2 = currentPosition2 + 2;
-    document.documentElement.style.setProperty("--move-div-2", `${currentPosition2.toString()}%`)
-
-    let currentPosition3 = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-3"))
-    console.log(currentPosition3);
-    currentPosition3 = currentPosition3 + 6;
-    document.documentElement.style.setProperty("--move-div-3", `${currentPosition3.toString()}%`)
-
-    let currentPosition4 = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--move-div-4"))
-    console.log(currentPosition4);
-    currentPosition4 = currentPosition4 - 8;
-    document.documentElement.style.setProperty("--move-div-4", `${currentPosition4.toString()}%`)
+                updatePosition(1, -moveAmount1, true);
+                updatePosition(2, -moveAmount2, false);
+                updatePosition(3, -moveAmount3, true);
+                updatePosition(4, -moveAmount4, false);
+            }
+        }
     }
-    console.log('agggggggggggggggg',num);
-}
-}
 
+    const updatePosition = (divNumber, moveAmount, isXAxis) => {
+        let currentPositionX = parseFloat(getComputedStyle(document.documentElement).getPropertyValue(`--move-div-${divNumber}-x`));
+        let currentPositionY = parseFloat(getComputedStyle(document.documentElement).getPropertyValue(`--move-div-${divNumber}-y`));
 
+        if (isXAxis) {
+            currentPositionX += moveAmount;
+        } else {
+            currentPositionY += moveAmount;
+        }
 
+        document.documentElement.style.setProperty(`--move-div-${divNumber}-x`, `${currentPositionX}%`);
+        document.documentElement.style.setProperty(`--move-div-${divNumber}-y`, `${currentPositionY}%`);
+    }
 
-    return(
+    return (
         <>
-        <div onWheel={scroll} className="float-area">
-            <div className="flo-div div-1">
-                <h2>This is the floating text</h2>
-            </div>
+            <div onWheel={scroll} className="float-area">
+                <div className="flo-div div-1">
+                    <h2>This is the floating text</h2>
+                </div>
 
-            <div className="flo-div div-2">
-                <h2>This is the floating text</h2>
-            </div>
+                <div className="flo-div div-2">
+                    <h2>This is the floating text</h2>
+                </div>
 
-            <div className="flo-div div-3">
-                <h2>This is the floating text</h2>
-            </div>
+                <div className="flo-div div-3">
+                    <h2>This is the floating text</h2>
+                </div>
 
-            <div className="flo-div div-4">
-                <h2>This is the floating text</h2>
+                <div className="flo-div div-4">
+                    <h2>This is the floating text</h2>
+                </div>
             </div>
-        </div>
         </>
     );
 };
